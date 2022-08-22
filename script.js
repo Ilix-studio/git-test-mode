648c767e5f1a9bba45e3122d74c0da208c43a010
Ilix-studio <ilishjyoti17@gmail.com>
2022-08-22 10:27:49 +0530

    third

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-const butt = document.getElementById("yes");
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click', function onClick(event) {
+  // 👇️ change background color
+  document.body.style.backgroundColor = 'salmon';
+
+  // 👇️ optionally change text color
+  // document.body.style.color = 'white';
+});
